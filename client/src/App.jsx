bee1d253939a44aa112fa7502a86ec337638a313
Ayushgit19 <146693918+Ayushgit19@users.noreply.github.com>
2025-07-09 23:49:23 +0530

    Hotel

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/home'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
+
+
+
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
+  return (
+    <div>
+      {!isOwnerPath && <NavBar />}
+      <HotelReg />
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/rooms' element={<AllRooms />}/>
+          <Route path='/rooms/:id' element={<RoomDetails />}/>
+          <Route path='/my-bookings' element={<MyBookings />}/>
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
