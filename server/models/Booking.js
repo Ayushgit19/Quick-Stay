f5b62ccd1ffef65f836c3f5704caae447f29cd39
Ayushgit19 <146693918+Ayushgit19@users.noreply.github.com>
2025-07-22 22:42:16 +0530

    sam

---
+++ server/models/Booking.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    user: {type: String, ref: "User", requiered: true},
+    room: {type: String, ref: "Room", requiered: true},
+    hotel: {type: String, ref: "Hotel", requiered: true},
+    checkInDate: {type: Date, requiered: true},
+    checkOutDate: {type: Date, requiered: true},
+    totalPrice: {type: Number, requiered: true},
+    guests: {type: Number, requiered: true},
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "cancelled"],
+        default: "pending",
+    },
+    paymentMethod: {type: String, requiered: true, default: "Pay at Hotel"},
+    isPaid: {type: Boolean, requiered: false},
+
+}, {timestamps: true})
+
+const Booking = mongoose.model("Room", bookingSchema)
+
+export default Booking
