c1c16f14fb8d85a9f3a08e0bff6597a2efd327ab
Ayushgit19 <146693918+Ayushgit19@users.noreply.github.com>
2025-07-24 22:27:30 +0530

    Update User.js

--- temp-server/models/User.js
+++ temp-server/models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 console.log("User hit");
 const userSchema = mongoose.Schema({
+    _id: {type: String, required: true},
     username: {type: String, required: true},
     email: {type: String, required: true},
     image: {type: String, required: true},
